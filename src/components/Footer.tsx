9aab4cc91c0be5b9b8683ab4ced85961f6ee6154
badiskefi <badiskefi@gmail.com>
2023-12-20 21:24:59 +0100

    Footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div className="bg-blackish text-center text-gray-500 py-4 pb-16 md:pb-4">
+      Copyright &copy; Logo <br /> All Rights Reserved 2024.
+    </div>
+  );
+}
+
+export default Footer;
